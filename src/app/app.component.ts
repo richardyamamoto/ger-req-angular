8db010f7dd871ba9e80e9cde7bc852256a50c1e5
Richard Yamamoto <rybolteri@gmail.com>
2019-10-21 21:11:19 -0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ecommerce';
+  title = 'teste';
+  products = [];
+  textInput = '';
+
+
+  handleClick(event) {
+    event.preventDefault();
+    this.products.push({
+      title:this.textInput,
+    })
+  }
 }
